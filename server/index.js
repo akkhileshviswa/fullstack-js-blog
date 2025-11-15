4f7db2904338b60cf3eb2a31609f38e1b9ffbd53
akkhileshviswa <akkhilesh24@gmail.com>
2025-11-15 08:29:37 +0530

    fix: add frontend url as constant

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const __dirname = process.cwd();
 app.use(helmet());
 
 app.use(cors({
-	origin: "http://localhost:3000",
+	origin: process.env.FRONTEND_URL,
 	methods: ["GET", "POST" , "PUT", "DELETE"],
 	credentials: true,
 }));
